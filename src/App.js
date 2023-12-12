5133af113eb926342e18e980e2dee3f73ffd4681
noobDevUmar <m.umarqureshi091@gmail.com>
2023-12-12 01:16:39 -0800

    Pagination and product page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import { Counter } from './features/counter/Counter';
-import './App.css';
-import ProductList from './features/product-list/ProductList';
+
+import HomePage from './pages/Home';
 function App() {
   return (
     <div className="App">
- <ProductList/>
+<HomePage/>
     </div>
   );
 }
